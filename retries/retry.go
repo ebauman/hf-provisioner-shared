776c342f38e8739541ab5dcb98e031c98c826129
Eamon Bauman <eamon@eamonbauman.com>
2023-08-25 14:14:35 -0500

    improved retry implementation

--- retries/retry.go
+++ retries/retry.go
@@ -2,14 +2,18 @@ package retries
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/json"
 	"time"
 )
 
 type GenericRetry struct {
-	Action            string      `json:"action"`
+	Name              string      `json:"action"`
 	MaxRetries        int         `json:"maxRetries"`
 	Attempts          int         `json:"attempts"`
-	LastAttemptTime   string      `json:"lastAttemptTime"`
+	BackoffTime       v1.Duration `json:"backoffTime"`
+	LastAttemptTime   v1.Time     `json:"lastAttemptTime"`
 	LastAttemptResult RetryResult `json:"lastAttemptResult"`
 }
 
@@ -18,75 +22,118 @@ type RetryResult struct {
 	Message string `json:"message"`
 }
 
-type Getter interface {
-	GetRetries() []GenericRetry
+type GetAnnotations interface {
+	GetAnnotations() map[string]string
 }
 
-type Setter interface {
-	SetRetries([]GenericRetry)
+type SetAnnotations interface {
+	SetAnnotations(map[string]string)
 }
 
 type Retrier interface {
-	Getter
-	Setter
+	GetAnnotations
+	SetAnnotations
 }
 
-func NewRetry(name string, maxRetries int) GenericRetry {
+func New(name string, maxRetries int) GenericRetry {
 	return GenericRetry{
-		Action:     name,
+		Name:       name,
 		MaxRetries: maxRetries,
 	}
 }
 
-func (r GenericRetry) CanRetry(obj Retrier) bool {
-	var instance = findOrCreateRetry(obj, r)
-	if instance != nil {
-		if instance.MaxRetries == 0 {
-			return true
-		}
-
-		if instance.Attempts < instance.MaxRetries {
-			return true
-		}
-	}
-
-	return false
+func (r GenericRetry) Success(obj Retrier) {
+	r.SetAttempt(obj, true, "")
 }
 
-func (r GenericRetry) Success(obj Retrier, message string) {
-	r.set(obj, true, message)
-}
-
-func (r GenericRetry) Failure(obj Retrier, message string) {
-	r.set(obj, false, message)
+func (r GenericRetry) Failure(obj Retrier) {
+	r.SetAttempt(obj, false, "")
 }
 
 func (r GenericRetry) Successf(obj Retrier, message string, args ...any) {
-	r.set(obj, true, fmt.Sprintf(message, args))
+	r.SetAttempt(obj, true, message, args...)
 }
 
 func (r GenericRetry) Failuref(obj Retrier, message string, args ...any) {
-	r.set(obj, false, fmt.Sprintf(message, args))
+	r.SetAttempt(obj, false, message, args...)
 }
 
-func (r GenericRetry) set(obj Retrier, success bool, message string) {
-	var instance = findOrCreateRetry(obj, r)
-	if instance != nil {
-		instance.Attempts += 1
-		instance.LastAttemptTime = time.Now().String()
-		instance.LastAttemptResult.Success = success
-		instance.LastAttemptResult.Message = message
+func (r GenericRetry) SetAttempt(obj Retrier, success bool, message string, args ...any) {
+	retryInstance, err := findOrCreateRetry(obj, r)
+	if err != nil {
+		return
+	}
+
+	retryInstance.Attempts++
+	retryInstance.LastAttemptTime = v1.Now()
+	retryInstance.LastAttemptResult = RetryResult{
+		Success: success,
+		Message: fmt.Sprintf(message, args...),
 	}
 }
 
-func findOrCreateRetry(obj Retrier, r GenericRetry) *GenericRetry {
-	for _, gr := range obj.GetRetries() {
-		if gr.Action == r.Action {
-			return &gr
+func (r GenericRetry) ExceededRetries(obj Retrier) (exceeded bool, ok bool) {
+	retryInstance, err := findOrCreateRetry(obj, r)
+
+	if err != nil {
+		logrus.Errorf("error executing exceededretries: %w", err)
+		return false, false
+	}
+
+	if retryInstance.MaxRetries >= retryInstance.Attempts {
+		return true, true
+	}
+
+	return false, true
+}
+
+func (r GenericRetry) CanRetry(obj Retrier) bool {
+	// does this retry exist on the object?
+	retryInstance, err := findOrCreateRetry(obj, r)
+	if err != nil {
+		logrus.Errorf("error executing canretry: %w", err)
+		return false
+	}
+
+	if retryInstance.MaxRetries >= retryInstance.Attempts {
+		return false
+	}
+
+	if r.LastAttemptTime.Time.Add(r.BackoffTime.Duration).Before(time.Now()) {
+		// if the last time we attempted this, plus the backoff duration, has not yet passed
+		return false
+	}
+
+	return true
+}
+
+func findOrCreateRetry(obj Retrier, r GenericRetry) (*GenericRetry, error) {
+	annos := obj.GetAnnotations()
+
+	// if the retry is not found, create it
+	annotationJson, ok := annos[r.Name]
+	if !ok {
+		annoJson, err := json.Marshal(r)
+		if err != nil {
+			return nil, fmt.Errorf("error marshalling genericretry: %s", err)
 		}
+
+		if len(annos) == 0 {
+			annos = map[string]string{}
+		}
+		annos[r.Name] = string(annoJson)
+
+		obj.SetAnnotations(annos)
+
+		return &r, nil
 	}
 
-	obj.SetRetries(append(obj.GetRetries(), r))
+	// the retry was found! return it after unmarshalling
+	var returnRetry = &GenericRetry{}
+	err := json.Unmarshal([]byte(annotationJson), returnRetry)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling genericretry: %w", err)
+	}
 
-	return &obj.GetRetries()[len(obj.GetRetries())-1]
+	return returnRetry, nil
 }
